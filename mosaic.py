8d845ffc0213278e38f93556e173ab20c34d5b39
AnshDesai <anshdesai20@gmail.com>
2022-08-12 18:06:31 -0400

    Changes for adding white frame!

--- mosaic.py
+++ mosaic.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 from natsort import natsorted
-import time
 
 
 class Reader:
@@ -34,18 +33,9 @@ class Reader:
             print(e)
 
     def get_json(self):
-
         return self.timeline[self.left_ind], self.timeline[self.right_ind]
-    # def read_json(self):
-    #     # Return timestamp from json
-    #     left_tstamp = self.timeline[self.left_ind]
-    #     right_tstamp = self.timeline[self.right_ind]
-    #     self.left_ind += 1
-    #     self.right_ind += 1
-    #     return left_tstamp, right_tstamp
 
     def get_frame(self):
-
         return self.left_frame, self.right_frame
 
     def read_frame(self):
@@ -62,25 +52,10 @@ class Reader:
     def __str__(self):
         # self.timeline = self.read_json()
         return "Sensor {} camera {}".format(str(self.sensor_id), str(self.camera_id))
-#
-# def test(filename):
-#     # print(cv2.getBuildInformation())
-#     gstStr = "filesrc location=%s ! image/jpeg,width=2592,height=1944,framerate=15/1 ! jpegdec ! videoconvert ! video/x-raw, format=BGRx ! videoconvert ! video/x-raw, format=BGR ! queue ! appsink" % filename
-#
-#     #     print(gstStr)
-#     vid = cv2.VideoCapture(gstStr, cv2.CAP_GSTREAMER)
-#     # vid = cv2.VideoCapture(filename)
-#     _, img = vid.read()
-#     print(img, img.shape)
-#     plt.imshow(img[:,:,::-1])
-#     # cv2.imshow("Test", img)
-#     plt.show()
 
 
 if __name__ == "__main__":
-
-    root = "/home/summer/software/aug1/"                               # Root path to one recording session
-
+    root = "/home/summer/nec/Aug_1/"                               # Root path to one recording session
     session_id = input("Enter Session ID:")                        # Session ID
     threshold = int(input("Enter threshold for picking frames:"))  # Threshold for picking frames
     camera_readers = []                                            # List to store objects for each camera
@@ -90,11 +65,11 @@ if __name__ == "__main__":
     # the original 2 * HD resolution of the frame
     TwoHD = 2560
     # the resolution that we want
-    width, height = 640, 390
+    width, height = 640, 360
     # calculate how many times that we need to resize it by half
-    half_ops = TwoHD / width / 2
+    half_ops = TwoHD // width // 2
 
-    output_video = cv2.VideoWriter('mosaic.avi',
+    output_video = cv2.VideoWriter('test.avi',
                                    cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),  # Writer object
                                    frame_rate, (width * 3, height * 3))
 
@@ -123,27 +98,25 @@ if __name__ == "__main__":
             left_fr, right_fr = cam.get_frame()
 
             if np.abs(time - left_tstamp) < np.abs(time - right_tstamp):
-                # print("Reading good frame at time stamp is:{} for cam {}".format(timestamp, cam))
-                if np.abs(time - left_tstamp < threshold):
+                if np.abs(time - left_tstamp) < threshold:
                     for i in range(half_ops):
                         left_fr = left_fr[0::2, 0::2, :]
-                        output_array.append(left_fr)
+                    output_array.append(left_fr)
                 else:
                     output_array.append(np.zeros([height, width, 3], dtype=np.uint8))
             else:
-                if np.abs(time - right_tstamp < threshold):
+                if np.abs(time - right_tstamp) < threshold:
                     for i in range(half_ops):
                         right_fr = right_fr[0::2, 0::2, :]
-                        output_array.append(right_fr)
-                        cam.read_frame()
+                    output_array.append(right_fr)
+                    cam.read_frame()
                 else:
                     output_array.append(np.zeros([height, width, 3], dtype=np.uint8))
-        # print(output_array[0].shape, output_array[1].shape)
-        # exit(0)
         white_frame = np.zeros([height, width, 3], dtype=np.uint8)
         white_frame.fill(255)
-        cv2.putText(white_frame, 'Timestamp:'+str(time), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 0), 3)
-        cv2.putText(white_frame, 'FrameId:'+str(c), ( 100, 200), cv2.FONT_HERSHEY_SIMPLEX, 3, (0, 0, 0), 3)
+        cv2.putText(white_frame, 'Timestamp:'+str(time), (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        cv2.putText(white_frame, 'FrameId:'+str(c), (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+
         row1 = np.concatenate((white_frame, output_array[0], output_array[1]), axis=1)
         # print(row1.shape)
         row2 = np.concatenate((output_array[2], output_array[3], output_array[4]), axis=1)
@@ -155,5 +128,10 @@ if __name__ == "__main__":
         c += 1
         if c % 50 == 0:
             print("{} frames have been wrote!".format(c))
-        if c == 10:
+        if c == 20:
             exit(0)
+
+        # plt.figure("Timelines for cameras")
+        # for cam in camera_readers:
+        #     plt.plot(cam.timeline, ".")
+        # plt.show()
