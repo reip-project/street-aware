b4c153bf578bce79acbc3d8834d4e2d91a7ae8c3
ypiadyk <yurii.piadyk@gmail.com>
2023-06-07 16:12:10 -0400

    Preliminary cameras position

--- reconstruction/camera_positions.py
+++ reconstruction/camera_positions.py
@@ -29,7 +29,7 @@ def axis_equal_3d(ax, zoom=1):
         getattr(ax, 'set_{}lim'.format(dim))(ctr - r/zoom, ctr + r/zoom)
 
 
-def plot_3d(R, T, figure_name="3d", title=None, size=(16, 9), axis_equal=True, save_as=None, **kw):
+def plot_3d(R, T, figure_name="3d", title=None, size=(9, 8), axis_equal=True, save_as=None, **kw):
     plt.figure(figure_name, size)
     plt.clf()
     ax = plt.subplot(111, projection='3d', proj_type='ortho')
@@ -68,9 +68,17 @@ def drawlines(img1,img2,lines,pts1,pts2):
 
 
 path = "./"
-files = ["zebra.json", "random.json"]
+files = ["zebra.json", "random.json", "volume.json"]
+# files = ["random.json"]
 
+# s1, s2 = ("sensor2", "left"), ("sensor1", "left")
+# s1, s2 = ("sensor2", "right"), ("sensor1", "left")
+# s1, s2 = ("sensor2", "left"), ("sensor2", "right")
 s1, s2 = ("sensor1", "left"), ("sensor3", "left")
+# s1, s2 = ("sensor4", "right"), ("sensor3", "left")
+# s1, s2 = ("sensor3", "left"), ("sensor3", "right")
+# s1, s2 = ("sensor3", "right"), ("sensor3", "left")
+# s1, s2 = ("sensor1", "left"), ("sensor1", "right")
 
 ps1, ps2 = [], []
 
@@ -90,21 +98,22 @@ for file in files:
             ps1.append(p1)
             ps2.append(p2)
 
+ps1, ps2 = np.array(ps1, dtype=np.float32), np.array(ps2, dtype=np.float32)
+# ps1, ps2 = np.array(ps1, dtype=np.int32), np.array(ps2, dtype=np.int32)
 
 filename_template = "./%s_%s.png"
 img1 = cv2.imread(filename_template % (s1[0], s1[1]))[:, :, ::-1]
 img2 = cv2.imread(filename_template % (s2[0], s2[1]))[:, :, ::-1]
 
-ps1, ps2 = np.array(ps1, dtype=np.int32), np.array(ps2, dtype=np.int32)
-
-K1 = np.array(json.load(open("./calibration_data/left_1.json"))["mtx"])
-K2 = np.array(json.load(open("./calibration_data/left_3.json"))["mtx"])
+filename_template = "./calibration_data/%s_%s.json"
+K1 = np.array(json.load(open(filename_template % (s1[1], s1[0][-1])))["mtx"])
+K2 = np.array(json.load(open(filename_template % (s2[1], s2[0][-1])))["mtx"])
 
 print(K1)
 print(K2)
 
-F, mask = cv2.findFundamentalMat(ps1, ps2, cv2.FM_LMEDS)
-print("\n", F, "\n")
+F, mask = cv2.findFundamentalMat(ps1, ps2, cv2.FM_RANSAC, ransacReprojThreshold=10, confidence=0.999, maxIters=1000)
+print("\n", F, "\n", mask)
 # We select only inlier points
 nps1 = ps1[mask.ravel()==1]
 nps2 = ps2[mask.ravel()==1]
@@ -112,39 +121,59 @@ nps2 = ps2[mask.ravel()==1]
 # Find epilines corresponding to points in right image (second image) and
 # drawing its lines on left image
 lines1 = cv2.computeCorrespondEpilines(nps2.reshape(-1,1,2), 2, F)
-img1l, _ = drawlines(img1.copy(),img2.copy(),lines1.reshape(-1,3),nps1,nps2)
+img1l, img2l = drawlines(img1.copy(),img2.copy(),lines1.reshape(-1,3),nps1,nps2)
 # Find epilines corresponding to points in left image (first image) and
 # drawing its lines on right image
 lines2 = cv2.computeCorrespondEpilines(nps1.reshape(-1,1,2), 1, F)
-img2l, _ = drawlines(img2.copy(),img1.copy(),lines2.reshape(-1,3),nps2,nps1)
+img2r, img1r = drawlines(img2.copy(),img1.copy(),lines2.reshape(-1,3),nps2,nps1)
 
 E = np.matmul(K2.T, np.matmul(F, K1))
 R1, R2, T = cv2.decomposeEssentialMat(E)
 print(R1.shape, T.shape)
 print(R1, "\n", R2, "\n", T)
 
+ups1 = cv2.undistortPoints(ps1, K1, None)
+ups2 = cv2.undistortPoints(ps2, K2, None)
+newMtx = np.eye(3, dtype=np.float32)
+
+newE, newMask = cv2.findEssentialMat(ups1, ups2, newMtx)
+# print(newMask)
+ups1 = ups1[newMask.ravel()==1]
+ups2 = ups2[newMask.ravel()==1]
+
+# retval, R, t, mask = cv2.recoverPose(newE, ups1, ups2, newMtx)
+retval, R, t, mask = cv2.recoverPose(E, ps1, ps2, (K1 + K2)/2)
+print(retval, "of", ps1.shape[0], R, t)
+
+print("\n", E)
+print("\n", newE)
+
 # plt.figure("mask", (24, 16))
 # plt.imshow(mask)
 # plt.tight_layout()
 
-plt.figure("img1", (16, 9))
+plt.figure("img1", (9, 8))
 plt.imshow(img1l)
-# plt.plot(ps1[:, 0], ps1[:, 1], "r.")
+plt.plot(ps1[:, 0], ps1[:, 1], "ro", markersize=10, mfc='none')
 # plt.plot(nps1[:, 0], nps1[:, 1], "g.")
 plt.tight_layout()
 
-plt.figure("img2", (16, 9))
-plt.imshow(img2l)
-# plt.plot(ps2[:, 0], ps2[:, 1], "r.")
+plt.figure("img2", (9, 8))
+plt.imshow(img2r)
+plt.plot(ps2[:, 0], ps2[:, 1], "ro", markersize=10, mfc='none')
 # plt.plot(nps2[:, 0], nps2[:, 1], "g.")
 plt.tight_layout()
 
-
-
 ax = plot_3d(None, None)
 
-basis(ax, T.ravel(), R1.T)
-basis(ax, T.ravel(), R2.T)
+c = -np.matmul(R.T, t)
+
+basis(ax, c.ravel(), R.T)
+# basis(ax, t.ravel(), R.T)
+# basis(ax, T.ravel(), R1.T)
+# basis(ax, -T.ravel(), R1.T)
+# basis(ax, T.ravel(), R2.T)
+# basis(ax, -T.ravel(), R2.T)
 axis_equal_3d(ax)
 
 plt.show()
